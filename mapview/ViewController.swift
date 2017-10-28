95870117d8dce8d22c1f0a1803660e814ddfdf57
Cronabit 1 <cronabit1@Cronabits-Mac-mini.local>
2017-10-28 14:37:55 +0530

    initial commit

--- mapview/ViewController.swift
+++ mapview/ViewController.swift
@@ -7,19 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController{
+    @IBOutlet weak var bigMap: MKMapView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.1, 0.1)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(22.2949163,70.7583113)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        bigMap.setRegion(region, animated: true)
+        
+        let annotaion = MKPointAnnotation()
+        
+        annotaion.coordinate = location
+        annotaion.title = "CRONABIT TEC."
+        annotaion.subtitle = "NAKSHTRA 5"
+        
+        bigMap.addAnnotation(annotaion)
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
-}
-
+    
+    
+    }
